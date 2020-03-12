3442128999c9bb37b0f8a604c02b6e6ab1364196
Brendan Killen <brendankillen@gmail.com>
2020-03-12 22:18:56 +0000

    HR-Access demo app

--- target/classes/public/js/hraccess.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() 
-{
-	$('#a.nav-link').on('click', function () 
-	{		
-		//$('#a.nav-link').find( 'li.active' ).removeClass( 'active' );
-		$(this).parent( 'li' ).addClass( 'active' );
-	});
-});
